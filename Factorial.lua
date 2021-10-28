b23e124f91630d538c7f842cb57e3bb04e5c0b28
Tashrif Ahmad Taif <86658780+hijibijee@users.noreply.github.com>
2021-10-28 11:11:27 +0600

    Day - 1

--- Factorial.lua
+++ Factorial.lua
@@ -1,9 +1,14 @@
 --Factorial of N
 function fact (n)
-    if n == 0 then
-        return 1;
-    else 
-        return n * fact(n - 1);
+    if n >= 0 then
+        if n == 0 then
+            return 1
+        else 
+            return n * fact(n - 1)
+        end
+    else
+        print("Invalid argument") 
+        return -1;
     end
 end
 
